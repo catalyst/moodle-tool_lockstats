c9f75ddc693bf962d273249cbe7ad2e6151df745
Nicholas Hoobin <nicholashoobin@catalyst-au.net>
2017-03-30 10:27:56 +1100

    Issue #5, adding the reset handling.

---
+++ classes/form/reset.php
@@ -0,0 +1,66 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Form for resetting the history.
+ *
+ * @package    tool_lockstats
+ * @author     Nicholas Hoobin <nicholashoobin@catalyst-au.net>
+ * @copyright  2017 Catalyst IT
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+namespace tool_lockstats\form;
+
+if (!defined('MOODLE_INTERNAL')) {
+    die('Direct access to this script is forbidden.'); // It must be included from a Moodle page.
+}
+
+use html_writer;
+use moodleform;
+
+require_once($CFG->libdir . '/formslib.php');
+
+/**
+ * Form for resetting the history.
+ *
+ * @package    tool_lockstats
+ * @author     Nicholas Hoobin <nicholashoobin@catalyst-au.net>
+ * @copyright  2017 Catalyst IT
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class reset extends moodleform {
+    /**
+     * Form definition.
+     */
+    protected function definition() {
+        global $OUTPUT;
+        $mform    = $this->_form;
+
+        $warningmsg = get_string('form_reset_warning', 'tool_lockstats');
+
+        $html  = html_writer::start_div('warning');
+        $html .= $OUTPUT->notification($warningmsg, 'warning');
+        $html .= html_writer::end_div();
+
+        $mform->addElement('html', $html);
+
+        $buttonstr = get_string('form_reset_button', 'tool_lockstats');
+
+        $this->add_action_buttons(true, $buttonstr);
+
+    }
+}
---
+++ historyreset.php
@@ -0,0 +1,57 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Form for resetting the history.
+ *
+ * @package    tool_lockstats
+ * @author     Nicholas Hoobin <nicholashoobin@catalyst-au.net>
+ * @copyright  2017 Catalyst IT
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+
+require_once(dirname(__FILE__) . '/../../../config.php');
+require_once($CFG->dirroot .'/admin/tool/crawler/lib.php');
+require_once($CFG->libdir . '/adminlib.php');
+
+require_login();
+require_capability('moodle/site:config', context_system::instance());
+
+admin_externalpage_setup('tool_lockstats');
+
+$form = new \tool_lockstats\form\reset(null);
+
+$baseurl = new moodle_url('/admin/tool/lockstats/');
+
+if ($data = $form->get_data()) {
+
+    if ($form->is_submitted()) {
+        global $DB;
+
+        $DB->delete_records('tool_lockstats_locks');
+        $DB->delete_records('tool_lockstats_history');
+
+        redirect($baseurl);
+    }
+} else if ($form->is_cancelled()) {
+    redirect($baseurl);
+}
+
+echo $OUTPUT->header();
+echo $OUTPUT->heading(get_string('reset_header', 'tool_lockstats'));
+echo $form->display();
+echo $OUTPUT->footer();
